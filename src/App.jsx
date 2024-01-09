8440dda294dc0d938edac93f3f42b7b9084fc321
adalyn.kim <adalyn.kim@goorm.io>
2024-01-09 23:10:54 +0900

    feat(modal): 설문조사 formData 객체 생성 & form change 시 감지 함수 작업

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,63 @@ import { Card, EmptyView, Header, SurveyModal } from '@/components';
 import styles from './App.module.scss';
 
 function App() {
+	const [formData, setFormData] = useState({
+		name: '',
+		phone: '',
+		email: '',
+		agreements: {
+			personal: false,
+			marketing: false,
+			advertisement: false,
+		},
+		isMajor: false,
+		goorm: {
+			useGoorm: false,
+			service: {
+				EDU: false,
+				LEVEL: false,
+				DEVTH: false,
+				IDE: false,
+				EXP: false,
+			},
+			reason: '',
+		},
+		expects: {
+			1: false,
+			2: false,
+			3: false,
+			4: false,
+		},
+		review: '',
+	});
 	const [isOpen, setIsOpen] = useState(false);
+
+	const changeFormData = (newFormData) => {
+		setFormData({ ...formData, ...newFormData });
+	};
+
+	const checkAllAgreements = (checkAllOptions) => {
+		// 전체 동의 true면 formData의 agreements true로, false면 전체 해제
+		if (checkAllOptions) {
+			setFormData({
+				...formData,
+				agreements: {
+					personal: true,
+					marketing: true,
+					advertisement: true,
+				},
+			});
+		} else {
+			setFormData({
+				...formData,
+				agreements: {
+					personal: false,
+					marketing: false,
+					advertisement: false,
+				},
+			});
+		}
+	};
 
 	// 모달의 toggle 'X' button 제어하는 함수
 	const handleToggle = () => {
@@ -21,7 +77,13 @@ function App() {
 			<main className={styles.main}>
 				<EmptyView />
 				{isOpen && (
-					<SurveyModal isOpen={isOpen} handleToggle={handleToggle} />
+					<SurveyModal
+						isOpen={isOpen}
+						handleToggle={handleToggle}
+						formData={formData}
+						changeFormData={changeFormData}
+						checkAllAgreements={checkAllAgreements}
+					/>
 				)}
 			</main>
 		</div>
